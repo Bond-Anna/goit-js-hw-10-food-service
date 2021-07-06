d39601156a88dd34d82ea1cae1414f7a998b71cb
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-07-06 14:46:59 +0300

    восстанавливает тему после перезагрузки страницы

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -5,17 +5,32 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-// bodyEl.classList.add('Theme.LIGHT');
+themeAfterPageReload();
 
 inputEl.addEventListener('change', onInputChange);
-function onInputChange(event) {
-  if (inputEl.chacked) {
-    bodyEl.classList.remove(Theme.LIGHT);
+function onInputChange() {
+  if (inputEl.checked) {
     bodyEl.classList.add(Theme.DARK);
+    bodyEl.classList.remove(Theme.LIGHT);
     localStorage.setItem('theme', Theme.DARK);
   } else {
     bodyEl.classList.add(Theme.LIGHT);
     bodyEl.classList.remove(Theme.DARK);
     localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+function themeAfterPageReload() {
+  const savedTheme = localStorage.getItem('theme');
+  if (savedTheme === null) {
+    bodyEl.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  } else if (savedTheme === Theme.LIGHT) {
+    bodyEl.classList.add(Theme.LIGHT);
+    bodyEl.classList.remove(Theme.DARK);
+  } else if (savedTheme === Theme.DARK) {
+    bodyEl.classList.add(Theme.DARK);
+    bodyEl.classList.remove(Theme.LIGHT);
+    inputEl.checked = true;
   }
 }
